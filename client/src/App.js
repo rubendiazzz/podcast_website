6799dd3f294ee57c6265a9814ef0b017b64cae8b
Rubén Díaz Fraga <rubendfraga@gmail.com>
2023-12-26 03:41:45 +0100

    Add Sidebar component and enable dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './utils/Themes';
+import Sidebar from './components/sidebar';
 
 const Container = styled.div`
+	display: flex;
 	background: ${({ theme }) => theme.bg};
 	width: 100%;
 	height: 100vh;
+	overflow-x: hidden;
+	overflow-y: hidden;
 `;
 
 function App() {
 	//Hooks
-	const [darkmode, setDarkmode] = useState(false);
+	const [darkmode, setDarkmode] = useState(true);
 
 	return (
 		<ThemeProvider theme={darkTheme ? darkTheme : lightTheme}>
 			<Container className="App">
+				<Sidebar></Sidebar>
 				Podcast
 			</Container>
 		</ThemeProvider>
