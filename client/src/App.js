6d3eb82c30e2c06e50e57b76081167f35b60eea2
Rubén Díaz Fraga <rubendfraga@gmail.com>
2023-12-26 03:16:07 +0100

    Add dark mode functionality to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './utils/Themes';
+
+const Container = styled.div`
+	background: ${({ theme }) => theme.bg};
+	width: 100%;
+	height: 100vh;
+`;
+
 function App() {
-  return (
-    <div className="App">
-      Podcast
-    </div>
-  );
+	//Hooks
+	const [darkmode, setDarkmode] = useState(false);
+
+	return (
+		<ThemeProvider theme={darkTheme ? darkTheme : lightTheme}>
+			<Container className="App">
+				Podcast
+			</Container>
+		</ThemeProvider>
+	);
 }
 
 export default App;
